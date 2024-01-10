eb87f32869e19288aa58bd21f78af7aaeab71646
mwhirls <megan.worley@live.com>
2024-01-10 15:18:18 -0800

    Updates after running Ruff linter

--- download.py
+++ download.py
@@ -7,11 +7,9 @@ from pathlib import Path
 import os
 from io import BytesIO
 import urllib.request
-from pathlib import Path
-import os
 import certifi
-import urllib.request
 import constants
+
 
 def extract_bz2(archive_path, dest):
     with bz2.open(archive_path, "rb") as f:
@@ -19,40 +17,46 @@ def extract_bz2(archive_path, dest):
         with open(dest, "wb") as extractedf:
             extractedf.write(content)
 
+
 def extract_tar(archive_path, dest, format=""):
-    with tarfile.open(archive_path, f'r:{format}') as tar:
+    with tarfile.open(archive_path, f"r:{format}") as tar:
         names = tar.getnames()
         assert len(names) == 1, "expected single archived file"
         mfile = tar.extractfile(names[0])
         with open(dest, "wb") as extractedf:
             extractedf.write(mfile.read())
 
+
 def unzip(archive):
     archive_path = archive.download_path
     dest = archive.extract_dest
-    print(f'extracting {archive_path} to {dest}...')
+    print(f"extracting {archive_path} to {dest}...")
     ext = Path(archive_path).suffix
     os.makedirs(os.path.dirname(dest), exist_ok=True)
-    if ext == '.bz2':
+    if ext == ".bz2":
         stem = archive.download_path.stem
-        if stem.endswith('.tar'):
+        if stem.endswith(".tar"):
             extract_tar(archive_path, dest, "bz2")
         else:
             extract_bz2(archive_path, dest)
-    elif ext == '.tar':
+    elif ext == ".tar":
         extract_tar(archive_path, dest)
-    assert dest.is_file(), "extracted archive not found; unknown error occurred during extracted"
-    print('...complete!')
+    assert (
+        dest.is_file()
+    ), "extracted archive not found; unknown error occurred during extracted"
+    print("...complete!")
+
 
 def download_artifact(archive):
-    print(f'downloading latest archive from {archive.url}...')
+    print(f"downloading latest archive from {archive.url}...")
     context = ssl.create_default_context(cafile=certifi.where())
     with urllib.request.urlopen(archive.url, context=context) as response:
         resb = BytesIO(response.read())
         os.makedirs(os.path.dirname(archive.download_path), exist_ok=True)
         with open(archive.download_path, "wb") as f:
             f.write(resb.getbuffer())
-    print('...complete!')
+    print("...complete!")
+
 
 class Archive:
     def __init__(self, url, download_path, extract_dest):
@@ -60,21 +64,30 @@ class Archive:
         self.download_path = Path(download_path)
         self.extract_dest = Path(extract_dest)
 
-jpn_sentences = Archive('https://downloads.tatoeba.org/exports/per_language/jpn/jpn_sentences.tsv.bz2',
-                        f'{constants.TEMP_DIR}/jpn_sentences.tsv.bz2',
-                        constants.JPN_SENTENCES_PATH)
-eng_sentences = Archive('https://downloads.tatoeba.org/exports/per_language/eng/eng_sentences.tsv.bz2',
-                        f'{constants.TEMP_DIR}/eng_sentences.tsv.bz2',
-                        constants.ENG_SENTENCES_PATH)
-indices = Archive('https://downloads.tatoeba.org/exports/jpn_indices.tar.bz2',
-                  f'{constants.TEMP_DIR}/jpn_indices.tar.bz2',
-                  constants.INDICES_PATH)
+
+jpn_sentences = Archive(
+    "https://downloads.tatoeba.org/exports/per_language/jpn/jpn_sentences.tsv.bz2",
+    f"{constants.TEMP_DIR}/jpn_sentences.tsv.bz2",
+    constants.JPN_SENTENCES_PATH,
+)
+eng_sentences = Archive(
+    "https://downloads.tatoeba.org/exports/per_language/eng/eng_sentences.tsv.bz2",
+    f"{constants.TEMP_DIR}/eng_sentences.tsv.bz2",
+    constants.ENG_SENTENCES_PATH,
+)
+indices = Archive(
+    "https://downloads.tatoeba.org/exports/jpn_indices.tar.bz2",
+    f"{constants.TEMP_DIR}/jpn_indices.tar.bz2",
+    constants.INDICES_PATH,
+)
 
 archives = [jpn_sentences, eng_sentences, indices]
+
 
 def get_latest(archives):
     for archive in archives:
         download_artifact(archive)
         unzip(archive)
+
 
 get_latest(archives)
--- parse.py
+++ parse.py
@@ -7,28 +7,33 @@ import os
 import re
 import constants
 
-OUTPUT_PATH = 'dist/jpn_eng_examples.json'
+OUTPUT_PATH = "dist/jpn_eng_examples.json"
+
 
 class SentenceField(str, Enum):
-    ID = 'id'
-    LANG = 'lang'
-    TEXT = 'text'
+    ID = "id"
+    LANG = "lang"
+    TEXT = "text"
+
 
 class IndexField(str, Enum):
-    SENTENCE_ID = 'id'
-    TRANSLATION_ID = 'trans_id'
-    TEXT = 'text'
+    SENTENCE_ID = "id"
+    TRANSLATION_ID = "trans_id"
+    TEXT = "text"
+
 
 class Sentence:
     def __init__(self, id, text):
         self.id = id
         self.text = text
 
+
 class Index:
     def __init__(self, id, trans_id, text):
         self.id = id
         self.trans_id = trans_id
         self.text = text
+
 
 class Word:
     def __init__(self, headword, reading, sense, surface_form, checked):
@@ -38,46 +43,74 @@ class Word:
         self.surface_form = surface_form
         self.checked = checked
 
+
 def load_sentences(tsv_path):
     with open(tsv_path, "r", encoding="utf-8") as tsvf:
-        reader = csv.DictReader(tsvf, fieldnames=[SentenceField.ID, SentenceField.LANG, SentenceField.TEXT], delimiter="\t")
-        sentences = map(lambda row: Sentence(row[SentenceField.ID], row[SentenceField.TEXT]), reader)
+        reader = csv.DictReader(
+            tsvf,
+            fieldnames=[SentenceField.ID, SentenceField.LANG, SentenceField.TEXT],
+            delimiter="\t",
+        )
+        sentences = map(
+            lambda row: Sentence(row[SentenceField.ID], row[SentenceField.TEXT]), reader
+        )
         return list(sentences)
-    
+
+
 def load_indices(tsv_path):
     with open(tsv_path, "r", encoding="utf-8") as tsvf:
-        reader = csv.DictReader(tsvf, fieldnames=[IndexField.SENTENCE_ID, IndexField.TRANSLATION_ID, IndexField.TEXT], delimiter="\t")
-        indices = map(lambda row: Index(row[IndexField.SENTENCE_ID], row[IndexField.TRANSLATION_ID], row[IndexField.TEXT]), reader)
+        reader = csv.DictReader(
+            tsvf,
+            fieldnames=[
+                IndexField.SENTENCE_ID,
+                IndexField.TRANSLATION_ID,
+                IndexField.TEXT,
+            ],
+            delimiter="\t",
+        )
+        indices = map(
+            lambda row: Index(
+                row[IndexField.SENTENCE_ID],
+                row[IndexField.TRANSLATION_ID],
+                row[IndexField.TEXT],
+            ),
+            reader,
+        )
         return list(indices)
-    
+
+
 def to_dict(sentences):
     tuples = map(lambda s: (s.id, s), sentences)
     return dict(list(tuples))
+
 
 def load_sentence_dict(path):
     sentences = load_sentences(path)
     return to_dict(sentences)
 
+
 def load_indices_dict(path):
     indices = load_indices(path)
     return to_dict(indices)
 
+
 def trim_word(word_dict):
-    headword = word_dict['headword']
+    headword = word_dict["headword"]
     result = dict(headword=headword)
-    reading = word_dict['reading']
+    reading = word_dict["reading"]
     if reading:
-        result['reading'] = reading
-    sense = word_dict['sense']
+        result["reading"] = reading
+    sense = word_dict["sense"]
     if sense:
-        result['sense'] = sense
-    surface_form = word_dict['surface_form']
+        result["sense"] = sense
+    surface_form = word_dict["surface_form"]
     if surface_form:
-        result['surface_form'] = surface_form
-    checked = word_dict['checked']
+        result["surface_form"] = surface_form
+    checked = word_dict["checked"]
     if checked:
-        result['checked'] = True
+        result["checked"] = True
     return result
+
 
 def parse_word(word):
     # format "headword()[sense]": https://dict.longdo.com/about/hintcontents/tanakacorpus.html
@@ -86,27 +119,40 @@ def parse_word(word):
     assert match, "headword not found"
     return trim_word(match.groupdict())
 
+
 def parse_words(index):
     word_strs = index.text.split()
     return [parse_word(w) for w in word_strs]
+
 
 def to_json_entry(sentence, eng_sentences_dict, indices_dict):
     try:
         index = indices_dict[sentence.id]
         translation = eng_sentences_dict[index.trans_id]
         words = parse_words(index)
-        return dict(id=sentence.id, text=sentence.text, translation=translation.text, words=words)
+        return dict(
+            id=sentence.id,
+            text=sentence.text,
+            translation=translation.text,
+            words=words,
+        )
     except KeyError:
         return None
 
+
 def to_json(jpn_sentences_dict, eng_sentences_dict, indices_dict):
-    entries = [to_json_entry(v, eng_sentences_dict, indices_dict) for v in jpn_sentences_dict.values()]
+    entries = [
+        to_json_entry(v, eng_sentences_dict, indices_dict)
+        for v in jpn_sentences_dict.values()
+    ]
     return [x for x in entries if x is not None]
+
 
 def write_file(json_data, dest):
     os.makedirs(os.path.dirname(dest), exist_ok=True)
-    with open(dest, "w", encoding='utf-8') as f:
-        json.dump(json_data, f, ensure_ascii=False, separators=(',', ':'))
+    with open(dest, "w", encoding="utf-8") as f:
+        json.dump(json_data, f, ensure_ascii=False, separators=(",", ":"))
+
 
 jpn_sentences_dict = load_sentence_dict(constants.JPN_SENTENCES_PATH)
 eng_sentences_dict = load_sentence_dict(constants.ENG_SENTENCES_PATH)
