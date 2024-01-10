ac9a97b606fc7188664e89a08527b9102795a310
mwhirls <megan.worley@live.com>
2024-01-10 11:11:00 -0800

    Link Japanese sentences with their translation

--- parse.py
+++ parse.py
@@ -2,6 +2,11 @@
 
 import csv
 from enum import Enum
+import json
+
+JPN_SENTENCES_PATH = 'extracted/jpn_sentences.tsv'
+ENG_SENTENCES_PATH = 'extracted/eng_sentences.tsv'
+INDICES_PATH = 'extracted/jpn_indices.csv'
 
 class SentenceField(str, Enum):
     ID = 'id'
@@ -24,10 +29,6 @@ class Index:
         self.trans_id = trans_id
         self.text = text
 
-JPN_SENTENCES_PATH = 'extracted/jpn_sentences.tsv'
-ENG_SENTENCES_PATH = 'extracted/eng_sentences.tsv'
-INDICES_PATH = 'extracted/jpn_indices.csv'
-
 def load_sentences(tsv_path):
     with open(tsv_path, "r", encoding="utf-8") as tsvf:
         reader = csv.DictReader(tsvf, fieldnames=[SentenceField.ID, SentenceField.LANG, SentenceField.TEXT], delimiter="\t")
@@ -48,6 +49,24 @@ def load_sentence_dict(path):
     sentences = load_sentences(path)
     return to_dict(sentences)
 
+def load_indices_dict(path):
+    indices = load_indices(path)
+    return to_dict(indices)
+
+def to_json_entry(sentence, eng_sentences_dict, indices_dict):
+    try:
+        index = indices_dict[sentence.id]
+        translation = eng_sentences_dict[index.trans_id]
+        return dict(id=sentence.id, transId=translation.id, text=sentence.text, translationText=translation.text)
+    except KeyError:
+        return None
+
+def to_json(jpn_sentences_dict, eng_sentences_dict, indices_dict):
+    entries = [to_json_entry(v, eng_sentences_dict, indices_dict) for v in jpn_sentences_dict.values()]
+    return [x for x in entries if x is not None]
+
 jpn_sentences_dict = load_sentence_dict(JPN_SENTENCES_PATH)
 eng_sentences_dict = load_sentence_dict(ENG_SENTENCES_PATH)
-indices = load_indices(INDICES_PATH)
+indices_dict = load_indices_dict(INDICES_PATH)
+examples_json = to_json(jpn_sentences_dict, eng_sentences_dict, indices_dict)
+print(json.dumps(examples_json))
